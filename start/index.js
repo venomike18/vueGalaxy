65f6ba7105fe142912e728e42840bb2d75011b26
venomike18 <medas.michele@gmail.com>
2023-02-24 10:35:00 +0100

    Add properties to an image

--- start/index.js
+++ start/index.js
@@ -3,7 +3,16 @@ const app = Vue.createApp({
         return {
             productName: 'Book a Cruise to the Moon',
             productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+            
             // additional properties later
+            productImage: 'assets/cruise.jpg',
+            productImageDescription: 'An astronaut floats outside the window while you sit in comfort',
+            productImageStyle: {
+                'border-radius': '15px'
+            },
+
+
+
             imageSource: './media/sample.jpg',
             classObject: {
                 centered: true,
