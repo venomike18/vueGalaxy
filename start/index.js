043a9a9119a8fba79cffa18adf6a8d75a8a6dcc6
venomike18 <medas.michele@gmail.com>
2023-02-24 10:16:50 +0100

    Hello World

---
+++ start/index.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            productName: 'Book a Cruise to the Moon',
+            productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+            // additional properties later
+            imageSource: './media/sample.jpg'
+            /*classObject: {
+                centered: true,
+                active: true
+            }*/
+        }
+    },
+});
